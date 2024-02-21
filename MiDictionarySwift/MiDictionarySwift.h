04cf6f3a4a846aef6365583fefc5b7042f144c4c
TebogoSibiya <kapiten.bm@gmail.com>
2024-02-21 08:49:49 +0200

    Initial Commit

---
+++ MiDictionarySwift/MiDictionarySwift.h
@@ -0,0 +1,18 @@
+//
+//  MiDictionarySwift.h
+//  MiDictionarySwift
+//
+//  Created by Admin on 2024/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MiDictionarySwift.
+FOUNDATION_EXPORT double MiDictionarySwiftVersionNumber;
+
+//! Project version string for MiDictionarySwift.
+FOUNDATION_EXPORT const unsigned char MiDictionarySwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MiDictionarySwift/PublicHeader.h>
+
+
